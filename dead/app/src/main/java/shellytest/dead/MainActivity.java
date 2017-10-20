c40655f694133bfd13dc924cf0c0e3b3f10d979b
Shelly <cwang135@uottawa.ca>
2017-10-20 16:07:44 -0400

    implemented socket.io
    get to send and recieve message from server

--- dead/app/src/main/java/shellytest/dead/MainActivity.java
+++ dead/app/src/main/java/shellytest/dead/MainActivity.java
@@ -1,17 +1,59 @@
 package shellytest.dead;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-import com.github.nkzawa.emitter.Emitter;
+//import com.github.nkzawa.socketio.client.IO;
+//import com.github.nkzawa.socketio.client.Socket;
+//import com.github.nkzawa.emitter.Emitter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+//import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.URISyntaxException;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> messageList = new ArrayList<String>();
@@ -30,7 +72,11 @@ public class MainActivity extends AppCompatActivity {
                 messageList.add(e.toString());
             }
         }
+
+        mSocket.on(Socket.EVENT_CONNECT,onConnect);
+        mSocket.on("message", onNewMessage);
         mSocket.connect();
+        mSocket.emit("intro", "NAme");
 
 
         super.onCreate(savedInstanceState);
@@ -50,13 +96,55 @@ public class MainActivity extends AppCompatActivity {
         if (messageList.size() >6) {
             messageList.remove(0);
         }
-        message.setText("");
-        adapter.notifyDataSetChanged();
-        mSocket.emit("intro", "NAme");
-        mSocket.emit("newRequest", message.getText().toString());
 
+        adapter.notifyDataSetChanged();
+
+        mSocket.emit("message", message.getText().toString());
+        mSocket.emit("newRequest", message.getText().toString());
+        message.setText("");
+
+        //mSocket.on("")
 
     }
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+
+        }
+    };
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+//            String mess = (String) args[0];
+//
+//            messageList.add(mess);
+//            if (messageList.size() >6) {
+//                messageList.remove(0);
+//            }
+//
+//            adapter.notifyDataSetChanged();
+
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    String mess = (String) args[0];
+
+                    messageList.add(mess);
+                    if (messageList.size() >6) {
+                        messageList.remove(0);
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                }
+            });
+
+
+        }
+    };
 
 
 }
