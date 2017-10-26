f7be941ee6e88c79f372f5b2bd07de26ccec9984
Shelly <cwang135@uottawa.ca>
2017-10-26 15:24:10 -0400

    Most Current updates
    - can host rooms
    -can join default room
    user name
    UI almost up to date

---
+++ AppropriatelyMoist/app/build/generated/source/buildConfig/androidTest/debug/appocalypse/appropriatelymoist/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package appocalypse.appropriatelymoist.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "appocalypse.appropriatelymoist.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
